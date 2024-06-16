307e4584f3919632cf649548769c46e1e387f910
Gaurish Gangwar <gaurishgangwar@gmail.com>
2024-06-16 18:06:29 +0900

    Weird Algorithm

---
+++ introductory_problems/src/lib.rs
@@ -0,0 +1,16 @@
+mod p01_weird_algorithm;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ introductory_problems/src/p01_weird_algorithm/mod.rs
@@ -0,0 +1,64 @@
+fn f(mut n: i64) -> Vec<i64> {
+    let mut ret = Vec::new();
+    loop {
+        ret.push(n);
+        if n == 1 {
+            break;
+        } else if n % 2 == 0 {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+
+    ret
+}
+
+fn main() {
+    let mut buffer = String::new();
+    let _ = std::io::stdin().read_line(&mut buffer);
+    let n = buffer.trim().parse().unwrap();
+    for x in f(n).iter() {
+        print!("{} ", *x);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{io::BufRead, str::FromStr};
+
+    use super::*;
+
+    #[test]
+    fn test_from_file() {
+        let filepath = std::path::PathBuf::from_str("..")
+            .unwrap()
+            .join(file!())
+            .parent()
+            .unwrap()
+            .join("input.txt");
+        println!("{filepath:?}");
+        let file = std::fs::File::options().read(true).open(filepath).unwrap();
+        let mut lines = std::io::BufReader::new(file).lines();
+        let n_input = lines.next().unwrap().unwrap().parse::<i64>().unwrap();
+        println!("{n_input:?}");
+        for input_idx in 1..=n_input {
+            let n = lines.next().unwrap().unwrap().parse().unwrap();
+            let result: Vec<i64> = lines
+                .next()
+                .unwrap()
+                .unwrap()
+                .split_ascii_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect();
+            let my_result = f(n);
+            assert_eq!(
+                result,
+                my_result,
+                "Input {} failed, expected={result:?}, got={my_result:?}",
+                input_idx + 1
+            );
+        }
+        //assert!(false);
+    }
+}
