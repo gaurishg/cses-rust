2be2a792de8f64f523a6f4f05bdb97bf586ec705
Gaurish Gangwar <gaurishgangwar@gmail.com>
2024-07-24 22:28:14 +0900

    Shortest Routes I

--- 04_graph_algorithms/src/p08_shortest_routes_i/mod.rs
+++ 04_graph_algorithms/src/p08_shortest_routes_i/mod.rs
@@ -1,4 +1,7 @@
-use std::{io::Read, usize};
+use std::{
+    collections::{BinaryHeap, HashMap},
+    io::Read,
+};
 
 struct Edge {
     source: usize,
@@ -7,23 +10,29 @@ struct Edge {
 }
 
 fn f(n_vertices: usize, edges: Vec<Edge>) -> Vec<usize> {
+    let mut adj = vec![HashMap::new(); n_vertices];
+    for edge in edges {
+        adj[edge.source]
+            .entry(edge.target)
+            .and_modify(|weight| *weight = std::cmp::min(*weight, edge.weight))
+            .or_insert(edge.weight);
+    }
+
     let mut costs = vec![usize::MAX; n_vertices];
-    costs[0] = 0;
+    let mut heap = BinaryHeap::new();
+    heap.push((std::cmp::Reverse(0), 0));
 
-    for _ in 0..n_vertices - 1 {
-        let mut anything_changed = false;
-        for edge in edges.iter() {
-            let u = edge.source;
-            let v = edge.target;
-            let edge_weight = edge.weight;
-            if costs[u] != usize::MAX && costs[u] + edge_weight < costs[v] {
-                costs[v] = costs[u] + edge_weight;
-                anything_changed = true;
-            }
+    while !heap.is_empty() {
+        let (std::cmp::Reverse(cost), node) = heap.pop().unwrap();
+        if costs[node] < cost {
+            continue;
         }
-
-        if !anything_changed {
-            break;
+        costs[node] = cost;
+        for (&child, &weight) in &adj[node] {
+            if costs[child] > costs[node] + weight {
+                costs[child] = costs[node] + weight;
+                heap.push((std::cmp::Reverse(costs[child]), child));
+            }
         }
     }
 
@@ -66,5 +75,69 @@ mod test_shortest_routes_i {
 3 2 3
 1 3 4";
         assert_eq!(f(3, get_edges(s)), vec![0, 5, 2]);
+    }
+
+    #[test]
+    fn test_1() {
+        let s = "10 20
+8 5 1
+9 10 2
+7 9 8
+9 8 8
+10 9 9
+7 8 10
+8 9 2
+7 10 10
+4 5 8
+5 6 1
+4 2 1
+5 3 6
+10 7 3
+3 5 2
+5 4 7
+1 2 9
+2 3 2
+6 7 5
+3 4 10
+3 2 10";
+        assert_eq!(
+            f(10, get_edges(s)),
+            "0 9 11 20 13 14 19 29 27 29"
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<_>>()
+        );
+    }
+
+    #[test]
+    fn test_2() {
+        let s = "10 20
+5 6 4
+5 1 7
+7 4 4
+7 8 1
+8 9 3
+5 7 6
+2 3 2
+1 2 7
+7 9 5
+2 5 8
+8 5 7
+4 5 6
+6 7 6
+8 7 6
+6 2 2
+3 6 8
+6 4 7
+9 10 5
+5 2 1
+3 4 8";
+        assert_eq!(
+            f(10, get_edges(s)),
+            "0 7 9 17 15 17 21 22 25 30"
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<_>>()
+        );
     }
 }
