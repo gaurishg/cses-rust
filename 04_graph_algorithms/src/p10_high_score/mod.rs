dba02dde5c267d477a966b9b67a0bee5146d69fd
Gaurish Gangwar <gaurishgangwar@gmail.com>
2024-07-25 12:13:10 +0900

    High Score

---
+++ 04_graph_algorithms/src/p10_high_score/mod.rs
@@ -0,0 +1,227 @@
+use std::io::Read;
+
+struct Edge {
+    source: usize,
+    destination: usize,
+    weight: i64,
+}
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum State {
+    NotVisited,
+    InProgress,
+    CanBeReached,
+    CanNotBeReached,
+}
+
+fn can_be_reached_from_the_fist_node(
+    source: usize,
+    can_be_reached_from_first: &mut Vec<bool>,
+    adj: &Vec<Vec<usize>>,
+) {
+    if can_be_reached_from_first[source] {
+        return;
+    }
+
+    can_be_reached_from_first[source] = true;
+
+    for &dest in adj[source].iter() {
+        can_be_reached_from_the_fist_node(dest, can_be_reached_from_first, adj);
+    }
+}
+
+fn can_reach_the_last_node(source: usize, can_reach_last: &mut Vec<State>, adj: &Vec<Vec<usize>>) {
+    if can_reach_last[source] != State::NotVisited {
+        return;
+    }
+    can_reach_last[source] = State::InProgress;
+    for &dest in adj[source].iter() {
+        if can_reach_last[dest] == State::NotVisited {
+            can_reach_the_last_node(dest, can_reach_last, adj);
+        }
+        match can_reach_last[dest] {
+            State::NotVisited => {}
+            State::InProgress => {}
+            State::CanNotBeReached => {
+                can_reach_last[source] = State::CanNotBeReached;
+                return;
+            }
+            State::CanBeReached => {
+                can_reach_last[source] = State::CanBeReached;
+                return;
+            }
+        }
+    }
+    can_reach_last[source] = State::NotVisited;
+}
+
+fn f(n_vertices: usize, mut edges: Vec<Edge>) -> i64 {
+    let mut dp = vec![i64::MIN; n_vertices + 1];
+    let mut can_be_reached_from_first = vec![false; n_vertices + 1];
+    let mut can_reach_last = vec![State::NotVisited; n_vertices + 1];
+    let mut adj = vec![vec![]; n_vertices + 1];
+
+    for &Edge {
+        source,
+        destination,
+        ..
+    } in edges.iter()
+    {
+        adj[source].push(destination);
+    }
+
+    dp[1] = 0;
+    can_be_reached_from_the_fist_node(1, &mut can_be_reached_from_first, &adj);
+    can_reach_last[n_vertices] = State::CanBeReached;
+    for node in 1..n_vertices {
+        if can_reach_last[node] == State::NotVisited {
+            can_reach_the_last_node(node, &mut can_reach_last, &adj);
+        }
+    }
+
+    edges.retain(
+        |&Edge {
+             source,
+             destination,
+             ..
+         }| {
+            can_be_reached_from_first[source] && can_reach_last[destination] == State::CanBeReached
+        },
+    );
+
+    for _ in 1..n_vertices {
+        for &Edge {
+            source,
+            destination,
+            weight,
+        } in edges.iter()
+        {
+            dp[destination] = dp[destination].max(dp[source].saturating_add(weight));
+        }
+    }
+
+    for Edge {
+        source,
+        destination,
+        weight,
+    } in edges
+    {
+        if dp[destination] < dp[source].saturating_add(weight) {
+            return -1;
+        }
+    }
+
+    dp[n_vertices]
+}
+
+pub fn main() {
+    let mut s = String::new();
+    _ = std::io::stdin().read_to_string(&mut s);
+
+    let (n_vertices, edges) = parse_input(&s);
+    println!("{}", f(n_vertices, edges));
+}
+
+fn parse_input(s: &str) -> (usize, Vec<Edge>) {
+    (
+        s.split_whitespace().next().unwrap().parse().unwrap(),
+        s.lines()
+            .skip(1)
+            .map(|l| l.split_whitespace())
+            .map(|mut it| Edge {
+                source: it.next().unwrap().parse().unwrap(),
+                destination: it.next().unwrap().parse().unwrap(),
+                weight: it.next().unwrap().parse().unwrap(),
+            })
+            .collect(),
+    )
+}
+
+#[cfg(test)]
+mod test_high_score {
+    use crate::p10_high_score::parse_input;
+
+    use super::f;
+
+    #[test]
+    fn example() {
+        let s = "4 5
+1 2 3
+2 4 -1
+1 3 -2
+3 4 7
+1 4 4";
+        let (n_vertices, edges) = parse_input(s);
+        assert_eq!(f(n_vertices, edges), 5);
+    }
+
+    #[test]
+    fn test_32() {
+        let s = "10 10
+1 9 -100000000
+9 10 1
+8 9 1
+7 8 1
+6 7 1
+5 6 1
+4 5 1
+3 4 1
+2 3 1
+10 2 1";
+        let (n, e) = parse_input(s);
+        assert_eq!(f(n, e), -1);
+    }
+
+    #[test]
+    fn test_33() {
+        let s = "6 8
+1 2 3
+2 3 -1
+3 2 5
+1 4 3
+4 5 -1
+5 4 5
+2 6 1
+1 6 1000000000";
+        let (n_vertices, edges) = parse_input(s);
+        assert_eq!(f(n_vertices, edges), -1);
+    }
+
+    #[test]
+    fn test_5() {
+        let s = "4 4
+1 2 1
+2 3 1
+3 2 1
+1 4 1";
+        let (n, e) = parse_input(s);
+        assert_eq!(f(n, e), 1);
+    }
+
+    #[test]
+    fn test_9() {
+        let s = "4 4
+1 4 1
+2 4 1
+2 3 1
+3 2 1
+";
+        let (n, e) = parse_input(s);
+        assert_eq!(f(n, e), 1);
+    }
+
+    #[test]
+    fn test_27() {
+        let s = "6 8
+1 2 -100
+1 3 1
+2 4 -100
+3 4 1
+4 5 -100
+4 6 1
+5 1 -100
+6 1 1";
+        let (n, e) = parse_input(s);
+        assert_eq!(f(n, e), -1);
+    }
+}
